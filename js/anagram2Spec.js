4281ad58cac1928ba4348cad164da32f327b82f8
Miah Clay <miahb88@gmail.com>
2022-05-27 23:02:17 -0400

    solved js

--- js/anagram2Spec.js
+++ js/anagram2Spec.js
@@ -1,8 +1,9 @@
 // Can you translate this driver code to unit tests?
 
-var ana = require("./anagram2"),
+var ana = require("./anagrams2"),
 listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
 
+anagrams_for("saltier", ["cognac", "saltier", "realist", "retails"]) == ["saltier", "realist", "retails"]
 console.log(ana.anagramsFor("threads", listOfWords).length == 4);
 console.log(ana.anagramsFor("threads", listOfWords)[0] == "threads");
 console.log(ana.anagramsFor("threads", listOfWords)[1] == "trashed");
--- js/anagrams2.js
+++ js/anagrams2.js
@@ -1,18 +1,37 @@
+listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
 
+function anagramsFor(str,arr) {
+let s = str.slice().split('')
+let answer = []
+let answer1 = []
+for (let j=0; j<arr.length; j++) {
+    let arr1 = []
+    for (let i=0; i<s.length; i++) {
+        let list = arr.slice()
+        if (list[j].includes(s[i])) {
+            arr1.push(s[i])} 
+    } answer.push(arr1)
+    // console.log(`word = ${arr.join('')}`)
+    // console.log(`matched word = ${listOfWords[j]}`)
+    if (arr1.join('').length == (arr[j]).length) {
+        answer1.push(arr[j])}} 
+    console.log(answer1)
+}
 
+console.log(anagramsFor("saltier", ["cognac", "saltier", "realist", "retails"]))
 console.log(anagramsFor("threads", listOfWords))
-console.log(anagramsFor("threads", listOfWords)[0])
-console.log(anagramsFor("threads", listOfWords)[1])
-console.log(anagramsFor("threads", listOfWords)[2])
-console.log(anagramsFor("threads", listOfWords)[3])
+// console.log(anagramsFor("threads", listOfWords)[0])
+// console.log(anagramsFor("threads", listOfWords)[1])
+// console.log(anagramsFor("threads", listOfWords)[2])
+// console.log(anagramsFor("threads", listOfWords)[3])
 
-console.log(anagramsFor("apple", listOfWords).length == 0);
+// console.log(anagramsFor("apple", listOfWords).length)
 
 
-console.log(anagramsFor("threads", listOfWords).length == 4);
-console.log(anagramsFor("threads", listOfWords)[0] == "threads");
-console.log(anagramsFor("threads", listOfWords)[1] == "trashed");
-console.log(anagramsFor("threads", listOfWords)[2] == "hardest");
-console.log(anagramsFor("threads", listOfWords)[3] == "hatreds");
+// console.log(anagramsFor("threads", listOfWords).length == 4);
+// console.log(anagramsFor("threads", listOfWords)[0] == "threads");
+// console.log(anagramsFor("threads", listOfWords)[1] == "trashed");
+// console.log(anagramsFor("threads", listOfWords)[2] == "hardest");
+// console.log(anagramsFor("threads", listOfWords)[3] == "hatreds");
 
-console.log(anagramsFor("apple", listOfWords).length == 0);
+// console.log(anagramsFor("apple", listOfWords).length == 0);
